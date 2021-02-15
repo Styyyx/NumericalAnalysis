9eff214d13780e51ee4ed9ecccad5161599a0cd0
Styyyx <46678157+Styyyx@users.noreply.github.com>
2021-02-15 10:45:16 +0800

    started project

---
+++ TaylorSeries/NumAnal_TaylorSeries/PolyTerm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumAnal_TaylorSeries
+{
+    // Class for Polynomial Terms
+    // A term is divided into: _sign, _coeffecient, _variable
+    public class PolyTerm
+    {
+        // + or - only
+        private char _sign;
+
+        // positive ints only
+        private int _coefficcient;
+
+        //may include '/' and '^'
+        //best to populate with '()'
+        private string _variable;
+
+        // if _coefficient == 1, it is removed in _textForm
+        private string _textForm;
+
+        public PolyTerm(char sign, int coefficient, string variable)
+        {
+            _sign = sign;
+            _coefficcient = coefficient;
+            _variable = variable;
+
+            if (_coefficcient != 1)
+            {
+                _textForm = $"{_sign} {_coefficcient}{_variable}";
+            }
+            else
+            {
+                _textForm = $"{_sign} {_variable}";
+            }
+        }
+
+        public string TextForm
+        {
+            get { return _textForm; }
+        }
+
+        public char Sign
+        {
+            get { return _sign; }
+        }
+
+        private int Coefficient
+        {
+            get { return _coefficcient; }
+        }
+    }
+}
---
+++ TaylorSeries/NumAnal_TaylorSeries/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumAnal_TaylorSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        ///<summary>
+        ///Generates a Taylor Series with the chosen function and up to number of terms based on degree input.
+        ///</summary>
+        ///
+        ///<param name="func">
+        ///Determines which function to use, takes only 1 or 2 as input:
+        /// 1 = cos(x),
+        /// 2 = sin(x)*e^x
+        ///</param>
+        ///
+        ///<param name="degree">
+        ///The degree to which the series will be generated to. 0 to pos infinity.
+        ///</param>
+        static public List<PolyTerm> GetTaylorSeries(int func, int degree)
+        {
+            List<PolyTerm> TS = new List<PolyTerm> { };
+
+            // cos(x)
+            if (func == 1)
+            {
+                TS.Add(new PolyTerm('+', 1, "cos(x)"));
+                
+                for (int i = 0; i < degree; i++)
+                {
+                    TS.Add(Derive(TS[i], 1));
+                }
+            }
+            // sin(x)*e^x
+            else if (func == 2)
+            {
+
+            }
+
+            return TS;
+        }
+
+        static public List<PolyTerm> GetPolynomial(int func, int degree)
+        {
+
+        }
+
+        static public PolyTerm Derive(PolyTerm x, int func)
+        {
+            string x_textForm = x.TextForm;
+
+            char tempSign = '+';
+            int tempCoeff = 0;
+            string tempVar = "";
+
+            if (func == 1)
+            {
+                if (x_textForm == "+ cos(x)")
+                {
+                    tempSign = '-';
+                    tempCoeff = 1;
+                    tempVar = "sin(x)";
+                }
+                else if (x_textForm == "- sin(x)")
+                {
+                    tempSign = '-';
+                    tempCoeff = 1;
+                    tempVar = "cos(x)";
+                }
+                else if (x_textForm == "- cos(x)")
+                {
+                    tempSign = '+';
+                    tempCoeff = 1;
+                    tempVar = "sin(x)";
+
+                }
+                else if (x_textForm == "+ sin(x)")
+                {
+                    tempSign = '+';
+                    tempCoeff = 1;
+                    tempVar = "cos(x)";
+                }
+            }
+            else if (func == 2)
+            {
+
+            }
+
+            return new PolyTerm(tempSign, tempCoeff, tempVar);
+        }
+    }
+}
