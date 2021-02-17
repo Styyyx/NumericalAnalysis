c1f86c3354894d93b7373f3761bdf3584f296973
Styyyx <46678157+Styyyx@users.noreply.github.com>
2021-02-17 22:08:19 +0800

    beepboop

--- TaylorSeries/NumAnal_TaylorSeries/Program.cs
+++ TaylorSeries/NumAnal_TaylorSeries/Program.cs
@@ -10,92 +10,197 @@ namespace NumAnal_TaylorSeries
     {
         static void Main(string[] args)
         {
-
+            GetTaylorSeries(2, 5, 5);
+            Console.ReadLine();
         }
 
         ///<summary>
         ///Generates a Taylor Series with the chosen function and up to number of terms based on degree input.
         ///</summary>
         ///
-        ///<param name="func">
+        ///<param name="whichFunction">
         ///Determines which function to use, takes only 1 or 2 as input:
         /// 1 = cos(x),
         /// 2 = sin(x)*e^x
         ///</param>
         ///
-        ///<param name="degree">
-        ///The degree to which the series will be generated to. 0 to pos infinity.
+        ///<param name="x">
+        ///The value to which the function will be evaluated to.
         ///</param>
-        static public List<PolyTerm> GetTaylorSeries(int func, int degree)
-        {
-            List<PolyTerm> TS = new List<PolyTerm> { };
+        /// 
+        ///<param name="n">
+        ///The degree to which the series will be generated to. Must be > 0. Default to 1
+        ///</param>
+        ///
+        ///<param name="a">
+        ///The point where the derivatives of the function are centered onto. Default to 0 for MacLaurin.
+        ///</param>
+        ///
 
-            // cos(x)
-            if (func == 1)
+
+        static public void GetTaylorSeries(int whichFunction, double x, int n= 1, double a = 0)
+        {
+            double TV = 0;
+            double EV = 0;
+            string Pn = "";
+            string currFunc;
+            double currCoeff = 0;
+            if (whichFunction == 1)
             {
-                TS.Add(new PolyTerm('+', 1, "cos(x)"));
-                
-                for (int i = 0; i < degree; i++)
+                TV = Math.Cos(x);
+                EV = Math.Cos(a);
+                Pn = $"{EV}";
+
+                currFunc = "cos(x)";
+
+                for (int i = 1; i <= n; i++)
                 {
-                    TS.Add(Derive(TS[i], 1));
+                    switch(currFunc)
+                    {
+                        case "cos(x)":
+                            {
+                                currFunc = "-sin(x)";
+                                currCoeff = -Math.Sin(a) / GetFactorial(i);
+                                break;
+                            }
+                        case "-sin(x)":
+                            {
+                                currFunc = "-cos(x)";
+                                currCoeff = -Math.Cos(a) / GetFactorial(i);
+                                break;
+                            }
+                        case "-cos(x)":
+                            {
+                                currFunc = "sin(x)";
+                                currCoeff = Math.Sin(a) / GetFactorial(i);
+                                break;
+                            }
+                        case "sin(x)":
+                            {
+                                currFunc = "cos(x)";
+                                currCoeff = Math.Cos(a) / GetFactorial(i);
+                                break;
+                            }
+                    }
+                    if (currCoeff != 0)
+                    {
+                        Pn += $" + ({currCoeff}x^{i})";
+                        EV += currCoeff * (Math.Pow(x, i));
+                    }
                 }
             }
-            // sin(x)*e^x
-            else if (func == 2)
+            else if (whichFunction == 2)
             {
+                TV = (Math.Pow(Math.E, x) * Math.Sin(x));
+                EV = (Math.Pow(Math.E, a) * Math.Sin(a));
+                Pn = $"{EV}";
 
+                /** Derivation Pattern
+                 * 
+                 * exsinx -> exsinx, excosx
+                 * excosx -> excosx, -exsinx
+                 * -exsinx -> -exsinx, -excosx
+                 * -excosx -> -excosx, exsinx
+                 * 
+                 * Only check for exsinx, excosx, -exsinx, -excosx
+                 */
+                List<string> currPolynomial = new List<string> { "exsinx" };
+
+                // Loop to nth degree
+                for (int i = 1; i <= n; i++)
+                {
+                    currCoeff = 0;
+
+                    // Derive
+                    List<string> newPolynomial = new List<string> { };
+                    for (int j = 0; j < currPolynomial.Count(); j++)
+                    {
+                        string currTerm = currPolynomial[j];
+
+                        //Product rule
+                        newPolynomial.Add(currTerm);
+
+                        switch(currPolynomial[j])
+                        {
+                            case "exsinx":
+                                {
+                                    newPolynomial.Add("excosx");
+                                    break;
+                                }
+                            case "excosx":
+                                {
+                                    newPolynomial.Add("-exsinx");
+                                    break;
+                                }
+                            case "-exsinx":
+                                {
+                                    newPolynomial.Add("-excosx");
+                                    break;
+                                }
+                            case "-excosx":
+                                {
+                                    newPolynomial.Add("exsinx");
+                                    break;
+                                }
+                        }
+                    }
+
+                    // Update current polynomial
+                    currPolynomial = newPolynomial;
+
+                    // Evaluate
+                    for (int k = 0; k < currPolynomial.Count(); k++)
+                    {
+                        switch (currPolynomial[k])
+                        {
+                            case "exsinx":
+                                {
+                                    currCoeff += (Math.Pow(Math.E, a) * Math.Sin(a));
+                                    break;
+                                }
+                            case "excosx":
+                                {
+                                    currCoeff += (Math.Pow(Math.E, a) * Math.Cos(a));
+                                    break;
+                                }
+                            case "-exsinx":
+                                {
+                                    currCoeff += (Math.Pow(Math.E, a) * -Math.Sin(a));
+                                    break;
+                                }
+                            case "-excosx":
+                                {
+                                    currCoeff += (Math.Pow(Math.E, a) * -Math.Cos(a));
+                                    break;
+                                }
+                        }
+                    }
+
+
+                    currCoeff /= GetFactorial(i);
+
+                    if (currCoeff != 0)
+                    {
+                        Pn += $" + ({currCoeff}x^{i})";
+                        EV += currCoeff * (Math.Pow(x, i));
+                    }
+                }
             }
 
-            return TS;
+            Console.WriteLine($"True Value: {TV}\nEstimated Value: {EV}\nTaylor Series: {Pn}");
         }
 
-        static public List<PolyTerm> GetPolynomial(int func, int degree)
+        static public int GetFactorial(int x)
         {
+            if (x == 0 || x == 1) { return 1; }
 
-        }
-
-        static public PolyTerm Derive(PolyTerm x, int func)
-        {
-            string x_textForm = x.TextForm;
-
-            char tempSign = '+';
-            int tempCoeff = 0;
-            string tempVar = "";
-
-            if (func == 1)
+            int sum = 1;
+            while (x > 0)
             {
-                if (x_textForm == "+ cos(x)")
-                {
-                    tempSign = '-';
-                    tempCoeff = 1;
-                    tempVar = "sin(x)";
-                }
-                else if (x_textForm == "- sin(x)")
-                {
-                    tempSign = '-';
-                    tempCoeff = 1;
-                    tempVar = "cos(x)";
-                }
-                else if (x_textForm == "- cos(x)")
-                {
-                    tempSign = '+';
-                    tempCoeff = 1;
-                    tempVar = "sin(x)";
-
-                }
-                else if (x_textForm == "+ sin(x)")
-                {
-                    tempSign = '+';
-                    tempCoeff = 1;
-                    tempVar = "cos(x)";
-                }
+                sum *= x;
+                x--;
             }
-            else if (func == 2)
-            {
-
-            }
-
-            return new PolyTerm(tempSign, tempCoeff, tempVar);
+            return sum;
         }
     }
 }
