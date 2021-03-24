deac26cc95ff568a81fe4f83bff85c9d9339230a
Styyyx <46678157+Styyyx@users.noreply.github.com>
2021-03-24 22:57:06 +0800

    add infixToPostfix() conversion

--- SecantMethod/secant.py
+++ SecantMethod/secant.py
@@ -1,12 +1,66 @@
+class Node:
+  def __init__(self, data):
+    self.left = None
+    self.right = None
+    self.data = data
+
+  def insert(self, data, side = None):
+    if (side == None):
+      if self.left == None:
+        self.left = Node(data)
+      elif self.right == None:
+        self.right = Node(data)
+
+def infixToPostfix(lst):
+  stack = []
+  output = []
+  for i in lst:
+    if i in '+-*/^':
+      if len(stack) == 0:
+        stack.append(i)
+      elif stack[-1] == '(':
+        stack.append(i)
+      elif i == '^':
+        stack.append(i)
+      elif i in '*/':
+        if stack[-1] in '+-':
+          stack.append(i)
+        else:
+          for j in range(len(stack)-1,0,-1):
+            if stack[j] in '*/^':
+              output.append(stack.pop())
+            else:
+              break
+          stack.append(i)
+      elif i in '+-':
+        for j in range(len(stack)-1, 0, -1):
+          if stack[j] in '+-*/^':
+            output.append(stack.pop())
+          else:
+            break
+        stack.append(i)
+    elif i == '(':
+      stack.append(i)
+    elif i == ')':
+      for j in range(len(stack)-1, 0, -1):
+        if stack[j] == '(':
+          stack.pop()
+          break
+        else:
+          output.append(stack.pop())
+    else:
+      output.append(i)
+
+  while len(stack) != 0:
+    output.append(stack.pop())
+  return output
+
 def parse(text:str):
   var = ''
-  varCount=0
   groupCount=0
   tree = []
   text.replace(' ', '')
-  print(text)
   for i in text:
-    print(i)
     if len(tree) == 0:
       tree.append(i)
     elif i.isdigit():
@@ -26,14 +80,17 @@ def parse(text:str):
             tree.append(i)
             groupCount -= 1
     elif i.isalpha():
-      varCount += 1
       if var == '':
         var = i
+        if tree[-1].isdigit():
+          tree.append('*')
         tree.append(i)
       elif i == var:
+        if tree[-1].isdigit():
+          tree.append('*')
         tree.append(i)
       else:
         print('Error: Cannot process more than one variable')
   return tree
 
-# print(parse('5x^2 + 4(1-x)'))
+print(''.join(i for i in infixToPostfix(parse('5x^2 + 4(1-x)'))))
