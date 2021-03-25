35a797d1cf523802d3b354437d579767b1c8b2e9
Styyyx <46678157+Styyyx@users.noreply.github.com>
2021-03-25 22:54:17 +0800

    Changes: fix: error in stack-to-output when converting infix to postfix remove: class Node add: evalPostfix() add: secant()

--- SecantMethod/secant.py
+++ SecantMethod/secant.py
@@ -1,62 +1,15 @@
-class Node:
-  def __init__(self, data):
-    self.left = None
-    self.right = None
-    self.data = data
+'''
+  Program Limits:
+    - Does not handle square roots.
 
-  def insert(self, data, side = None):
-    if (side == None):
-      if self.left == None:
-        self.left = Node(data)
-      elif self.right == None:
-        self.right = Node(data)
+'''
 
-def infixToPostfix(lst):
-  stack = []
-  output = []
-  for i in lst:
-    if i in '+-*/^':
-      if len(stack) == 0:
-        stack.append(i)
-      elif stack[-1] == '(':
-        stack.append(i)
-      elif i == '^':
-        stack.append(i)
-      elif i in '*/':
-        if stack[-1] in '+-':
-          stack.append(i)
-        else:
-          for j in range(len(stack)-1,0,-1):
-            if stack[j] in '*/^':
-              output.append(stack.pop())
-            else:
-              break
-          stack.append(i)
-      elif i in '+-':
-        for j in range(len(stack)-1, 0, -1):
-          if stack[j] in '+-*/^':
-            output.append(stack.pop())
-          else:
-            break
-        stack.append(i)
-    elif i == '(':
-      stack.append(i)
-    elif i == ')':
-      for j in range(len(stack)-1, 0, -1):
-        if stack[j] == '(':
-          stack.pop()
-          break
-        else:
-          output.append(stack.pop())
-    else:
-      output.append(i)
+from math import e, pi
 
-  while len(stack) != 0:
-    output.append(stack.pop())
-  return output
+var = ''
 
 def parse(text:str):
-  var = ''
+  global var
   groupCount=0
   tree = []
   text.replace(' ', '')
@@ -80,7 +33,11 @@ def parse(text:str):
             tree.append(i)
             groupCount -= 1
     elif i.isalpha():
-      if var == '':
+      if i == 'e':
+        tree.append(e)
+      elif i == 'pi':
+        tree.append(pi)
+      elif var == '':
         var = i
         if tree[-1].isdigit():
           tree.append('*')
@@ -93,4 +50,93 @@ def parse(text:str):
         print('Error: Cannot process more than one variable')
   return tree
 
-print(''.join(i for i in infixToPostfix(parse('5x^2 + 4(1-x)'))))
+def infixToPostfix(lst:list)->list:
+  stack = []
+  output = []
+  for i in lst:
+    if i in '+-*/^':
+      if len(stack) == 0:
+        stack.append(i)
+      elif stack[-1] == '(':
+        stack.append(i)
+      elif i == '^':
+        stack.append(i)
+      elif i in '*/':
+        if stack[-1] in '+-':
+          stack.append(i)
+        else:
+          for j in range(len(stack)-1, -1,-1):
+            if stack[j] in '*/^':
+              output.append(stack.pop())
+            else:
+              break
+          stack.append(i)
+      elif i in '+-':
+        for j in range(len(stack)-1, -1, -1):
+          if stack[j] in '+-*/^':
+            output.append(stack.pop())
+          else:
+            break
+        stack.append(i)
+    elif i == '(':
+      stack.append(i)
+    elif i == ')':
+      for j in range(len(stack)-1, 0, -1):
+        if stack[j] == '(':
+          stack.pop()
+          break
+        else:
+          output.append(stack.pop())
+    else:
+      output.append(i)
+
+  while len(stack) != 0:
+    output.append(stack.pop())
+  return output
+
+def evalPostfix(pf:list, varVal):
+  newpf = pf[:]
+  for i in range(len(newpf)):
+    if newpf[i].isdigit():
+      newpf[i] = float(newpf[i])
+    elif newpf[i] == var:
+      newpf[i] = varVal
+  stack = []
+  for i in newpf:
+
+    if str(i) in '+-*/^':
+      rightVal = stack.pop()
+      leftVal = stack.pop()
+      if i == '+':
+        stack.append(leftVal + rightVal)
+      elif i == '-':
+        stack.append(leftVal - rightVal)
+      elif i == '*':
+        stack.append(leftVal * rightVal)
+      elif i == '/':
+        stack.append(leftVal / rightVal)
+      elif i == '^':
+        stack.append(leftVal ** rightVal)
+    else:
+      stack.append(i)
+  return stack[0]
+
+def secant(x0, x1, func, iter=1, maxIter=None, maxErr=None):
+  if maxIter==None == maxErr==None:
+    return 'Error: Stopping requirements invalid'
+  fnx0 = evalPostfix(func, x0)
+  fnx1 = evalPostfix(func, x1)
+  x2 = x1 - fnx1 * ((x1-x0)/(fnx1-fnx0))
+  err = abs(x2-x1)
+  print([iter, x0, x1, fnx0, fnx1, x2, err])
+  if (iter != maxIter and maxIter != None) or (err > maxErr and maxErr != None):
+    secant(x1, x2, func, iter+1, maxIter, maxErr)
+
+# Activity 3 A.3
+fn = 'x^6-x-1'
+x0 = 1
+x1 = 1.5
+maxErr = 0.0001
+func = infixToPostfix(parse(fn))
+secant(x0, x1, func, maxErr=maxErr)
+
